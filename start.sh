1e773b0cba4cd1b6d2f5b1e67c39ff6deb7f325d
ProffisionalPXT <gabrielpxt98@gmail.com>
2026-08-20 08:59:30 -0300

    Fix: Aumenta timeout do Gunicorn para impedir que o Render reinicie o servidor por falta de CPU

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+﻿#!/bin/bash
 cd whatsapp-bot
-node --experimental-websocket index.js &
+node index.js &
 cd ..
-gunicorn app:app --bind 0.0.0.0:$PORT
+gunicorn app:app --bind 0.0.0.0:$PORT --timeout 300 --workers 1 --threads 4
