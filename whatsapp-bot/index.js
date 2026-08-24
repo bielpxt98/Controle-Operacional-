11ccf9c8159cba1c9dea77a91d12abdbbffbf715
ProffisionalPXT <gabrielpxt98@gmail.com>
2026-08-24 16:02:54 -0300

    Fix: Atualiza IA para gemini-3.5-flash pois o 2.5 esta com instabilidade no servidor do Google (Erro 500)

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -290,7 +290,7 @@ iniciarLoopCHEP();
 async function classifyImage(buffer, textCaption, isFromGroup) {
     try {
         console.log("[GEMINI] Analisando imagem recebida...");
-        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-3.5-flash" });
         
         let prompt = "";
         if (isFromGroup) {
